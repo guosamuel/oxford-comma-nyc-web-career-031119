e5f973d3b611ef388b6ebfbc5b6a762e0b683e82
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 15:47:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,8 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
+  else
+    last_item = array.pop
+    return array.join(", ").concat(", and #{last_item}") 
   end
 end
